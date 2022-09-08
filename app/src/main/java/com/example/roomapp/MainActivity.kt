f2006f266e8b27f6f9921d630d25d71c38cbffaf
wallace08797 <fenilahir16@gmail.com>
2022-09-08 19:51:17 +0530

    Firebase Dependencies implemented Successfully

--- app/src/main/java/com/example/roomapp/MainActivity.kt
+++ app/src/main/java/com/example/roomapp/MainActivity.kt
@@ -8,6 +8,8 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
